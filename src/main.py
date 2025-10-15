ad92ae91b628789a1ae5678fcde2eb289b442e0c
jjordan19 <jjordan1@vt.edu>
2025-10-15 19:34:00 -0400

    initialize main app

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request, Form
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return { "message": "Ticketing App!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
